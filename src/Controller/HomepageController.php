d8d5d04e9f258dcc7811a33af5b06a2ae56dae77
Koen Eelen <koen.eelenpubliq.be>
2024-12-11 20:57:04 +0100

    first controllers

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    #[Route('/info', name: 'app_info')]
+    public function info(): Response
+    {
+        return $this->render('homepage/info.html.twig', [
+            'controller_name' => 'HomepageController+info',
+        ]);
+    }
+}
