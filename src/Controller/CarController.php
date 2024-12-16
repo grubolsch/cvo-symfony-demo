e2beb9906ff58899e2e900d6bceb1e0826e335de
Koen Eelen <koen.eelenpubliq.be>
2024-12-16 20:21:00 +0100

    json view

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Car;
+use App\Repository\CarRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,7 +17,7 @@ class CarController extends AbstractController
     {
         $car = new Car();
         $car->setBrand('Audi');
-        $car->setColor('geel');
+        $car->setColor('rood');
         $car->setBuildyear(2000);
         $car->setName('Audi A3');
 
@@ -24,6 +26,23 @@ class CarController extends AbstractController
 
         return $this->render('car/index.html.twig', [
             'controller_name' => 'CarController',
+        ]);
+    }
+
+    #[Route('/car/view', name: 'app_car_view')]
+    public function view(CarRepository $carRepository): Response
+    {
+        $cars = $carRepository->findAll();
+
+        foreach ($cars as $car) {
+            //echo $car->getColor();
+            //echo '<br>';
+        }
+
+        // use Symfony\Component\HttpFoundation\JsonResponse;
+        return new JsonResponse([
+            'naam' => 'Koen',
+            'age' => 36
         ]);
     }
 }
